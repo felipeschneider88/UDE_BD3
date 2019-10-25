7d7e41f86fa6207442a286eb68003bc1ba84839a
javaPhpMyAdmin <chelobat16411@gmail.com>
2019-10-25 18:20:53 -0300

    Se agrega codigo a clase AccesoBD y Fachada
    Agregue algunas partes de las consultas que resuelve el AccesoBD, me quedan dos para pensar junto con la ultima la de obtenerDragQueenGanadora que esta para pensarla, tambien implemente algunos metodos de la Fachada, que obviamente algunas clases aun no las reconoce por no estar todo asociado dentro de un mismo Package anidado a un proyecto, en ambas clases agregue una serie de comentarios con algunas dudas o cosas que faltan con la etiqueta TODO, digo para que se pueda leer e intercambiar opiniones sobre algunas cosas ya que es nuevo para mi este lenguaje y quizas algo no quede bien o no se entienda.
    Mañana intentaria pensar bien los String de la clase Consultas que para mi son los escenciales y tienen que quedar bien de bien, aunque me gustaria crear la base y las dos tablas para probar las consultas con diversos casos y ver de ante mano que realizen bien su cometido.

--- Practico/Practico 2/Ejercicio3/AccesoBD/AccesoBD.java
+++ Practico/Practico 2/Ejercicio3/AccesoBD/AccesoBD.java
@@ -1,7 +1,227 @@
 import java.io.FileInputStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+
+import com.mysql.jdbc.PreparedStatement;
+
+import poolConexiones.Conexion;
+
+
+//TODO: FALTARIA ENLAZAR LA CLASE CONSULTAS QUE COMO NO ESTA EN NINGUN PACK NO PUEDE SER IMPORTADA, PERO ESO SE UNE TOOODO CUANDO HAGAMOS LOS PACKAGES CORRESPONDIENTES
 public class AccesoBD {
 	
+	//VERIFICA QUE EXISTA UNA TEMPORADA
+	//TODO: AGREGAR CLASE PERSISTENCIA EXCEPTION
+	//TODO: NO SE RECONOCE EL MEDOTO PREPARESTATEMENT QUE SE DERIVA DE LA VARIABLE CON, HABRIA QUE VER DE CREARLO MISMO EN EL POOL DE CONEXIONES
+	public boolean existeTemp(Conexion con, int nroTemp) throws PersistenciaException {
+		boolean existeTemp = false;
+		try
+		{ 
+			Consultas consultas= new Consultas(); 
+			String query= consultas.existeTemp();
+			PreparedStatement pstmt1 = con.prepareStatement(query);
+			pstmt1.setInt(1, nroTemp); 
+			ResultSet rs = pstmt1.executeQuery(); 
+			if(rs.next()) 
+				existeTemp = true; 
+			
+			rs.close();  
+			pstmt1.close();
 	
+		} catch(SQLException e) 
+		{
+			//TODO: ESTA BIEN LANZAR ESTA EXCEPCION AQUI? Y QUE PARAMETRO SE LE PASARIA EN CASO AFIRMATIVO, OSEA UNA VARIABLE CON UN MENSAJE A LA CAPA DE ARRIBA, LA FACHADA CONCRETAMENTE
+			throw new PersistenciaException();
+		}
+		return existeTemp;
+	}
+	
+	//VERIFICA QUE EXISTA UNA DRAGQUEEN
+	//TODO: DUDAS DE SI SE PASA MENSAJE A LA CLASE PERSISTENCIAEXCEPTION AL ENCONTRAR UN ERROR TRATANDO DE INSERTAR UN REGISTRO
+	public boolean existeDragQueen(Conexion con, int nroDrag) throws PersistenciaException {
+		
+		boolean existeDrag = false;
+		try
+		{ 
+			Consultas consultas= new Consultas(); 
+			String query= consultas.existeTemp();
+			PreparedStatement pstmt1 = con.prepareStatement(query);
+			pstmt1.setInt(1, nroDrag); 
+			ResultSet rs = pstmt1.executeQuery(); 
+			if(rs.next()) 
+				existeDrag = true; 
+			
+			rs.close();  
+			pstmt1.close();
+	
+		} catch(SQLException e) 
+		{
+			throw new PersistenciaException();
+		}
+		return existeDrag;
+	}
+	
+	//REGISTRA TEMPORADA VERIFICANDO QUE NO EXISTA ANTES
+	//TODO: DUDAS DE SI SE PASA MENSAJE A LA CLASE PERSISTENCIAEXCEPTION AL ENCONTRAR UN ERROR TRATANDO DE INSERTAR UN REGISTRO
+	public void insertarTemp(Conexion con, int nroTemp, int anio, int cantCapitulos) throws PersistenciaException {
+		
+		try
+		{ 
+			Consultas consultas= new Consultas(); 
+			String query= consultas.existeTemp();
+			PreparedStatement pstmt1 = con.prepareStatement(query);
+			pstmt1.setInt(1, nroTemp);
+			pstmt1.setInt(2, anio);
+			pstmt1.setInt(3, cantCapitulos);
+			ResultSet rs = pstmt1.executeQuery(); 
+			
+			rs.close();  
+			pstmt1.close();
+	
+		} catch(SQLException e) 
+		{
+			throw new PersistenciaException();
+		}
+	}
+	
+	//INSERTA DRAGQUEEN EN UNA TEMPORADA, VERIFICANDO QUE LA TEMPORADA QUE LE CORRESPONDE ESTE REGISTRADA
+	//TODO: DUDAS DE SI SE PASA MENSAJE A LA CLASE PERSISTENCIAEXCEPTION AL ENCONTRAR UN ERROR TRATANDO DE INSERTAR UN REGISTRO
+	public void insertarDragQueen(Conexion con, int nroTemp, String nombre) throws PersistenciaException {
+		
+		try
+		{ 
+			Consultas consultas= new Consultas(); 
+			String query= consultas.existeTemp();
+			PreparedStatement pstmt1 = con.prepareStatement(query);
+			pstmt1.setInt(1, nroTemp);
+			pstmt1.setString(2, nombre);
+			ResultSet rs = pstmt1.executeQuery(); 
+			
+			rs.close();  
+			pstmt1.close();
+	
+		} catch(SQLException e) 
+		{
+			throw new PersistenciaException();
+		}
+		
+	}
+	
+	
+	//DEVUELVE LSITADO DE TEMPORADAS ORDENADAS POR NUMERO DE TEMPORADA
+	//TODO: ME GENERA DUDA QUE SE HACE CON EL CAMPO SECUENCIA DE LA TABLA TEMPORADA
+	public List<VOTemporada> listarTemporadas(Conexion con) throws PersistenciaException {
+		
+		try
+		{ 
+			List<VOTemporada> listaTemporadas = new ArrayList<VOTemporada>();
+			VOTemporada VOT;
+			
+			Consultas consultas= new Consultas(); 
+			String query= consultas.existeTemp();
+			PreparedStatement pstmt1 = con.prepareStatement(query);
+			
+			ResultSet rs = pstmt1.executeQuery(); 
+			
+			while(rs.next()) {
+				int nroTemp = 0;
+				int anio = 0;
+				int cantCapitulos = 0;
+				
+				nroTemp = rs.getInt("nroTemp");
+				anio = rs.getInt("anio");
+				cantCapitulos = rs.getInt("cantCapitulos");
+				
+				VOT = new VOTemporada(nroTemp, anio, cantCapitulos);
+				
+				listaTemporadas.add(VOT);
+			}
+			
+			rs.close();  
+			pstmt1.close();
+	
+			return listaTemporadas;
+					
+		} catch(SQLException e) 
+		{
+			throw new PersistenciaException();
+		}
+	}
+	
+	
+	//DEVUELVE LISTADO DE DE TODAS LAS DRAGQUEENS DE UNA TEMPORADA
+	public List<VODragQueen> listarDragQueens(Conexion con, int nroTemp) throws PersistenciaException {
+		
+		//TODO: PARA RESOLVER
+		return null;
+	}
+	
+	//DEVUELVE LA TEMPORADA CON MAS PARTICIPANTES Y NUMERO CORRESPONDIENTE DE PARTICIPANTES, EN CASO DE EXISTIR DOS IGUALES DEVUELVE LA TEMP. DE NUMERO MAS ALTA
+	public VOTemporada tempMasParticipantes(Conexion con) throws PersistenciaException {
+		
+		try
+		{ 
+			VOTemporada VOT;
+			Consultas consultas= new Consultas(); 
+			String query= consultas.existeTemp();
+			PreparedStatement pstmt1 = con.prepareStatement(query);
+			
+			ResultSet rs = pstmt1.executeQuery(); 
+			
+			if(rs.next()) {
+				int nroTemp = 0;
+				int anio = 0;
+				int cantCapitulos = 0;
+				
+				nroTemp = rs.getInt("nroTemp");
+				anio = rs.getInt("anio");
+				cantCapitulos = rs.getInt("cantCapitulos");
+				
+				 VOT = new VOTemporada(nroTemp, anio, cantCapitulos);
+				
+			}
+			
+			rs.close();  
+			pstmt1.close();
+	
+			return VOT;
+					
+		} catch(SQLException e) 
+		{
+			throw new PersistenciaException();
+		}
+	}
+	
+	//AGREGA UNA VICTORIA A LA DRAGQUEEN CORRESPONDIENTE AL NUMERO DE TEMPORADA INDICADO
+	public void registrarVictoria(Conexion con, int nroTemp, int nroPart) throws PersistenciaException {
+		
+		try
+		{ 
+			Consultas consultas= new Consultas(); 
+			String query= consultas.existeTemp();
+			PreparedStatement pstmt1 = con.prepareStatement(query);
+			pstmt1.setInt(1, nroTemp);
+			pstmt1.setInt(2, nroPart);
+			ResultSet rs = pstmt1.executeQuery(); 
+			
+			rs.close();  
+			pstmt1.close();
+	
+		} catch(SQLException e) 
+		{
+			throw new PersistenciaException();
+		}
+		
+	}
+
+	//DEVUELVE LA DRAGQUEEN GANADORA DE UNA TEMPORADA, TAMBIEN LA CANTIDAD DE VICTORIAS DE LA GANADORA.
+	public VODragQueen obtenerGanadora(Conexion con) throws PersistenciaException {
+		
+		//TODO: PARA RESOLVER
+		return null;
+	}
+
 }
--- Practico/Practico 2/Ejercicio3/Fachada.java
+++ Practico/Practico 2/Ejercicio3/Fachada.java
@@ -1,5 +1,9 @@
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.List;
+
+import poolConexiones.Conexion;
+import poolConexiones.IConexion;
 
 public class Fachada {
 	
@@ -7,12 +11,123 @@ public class Fachada {
 		
 	}
 
-
-	public void nuevaTemporada(VOTemporada voT){}
+	//TODO: NO VA A ENCONTRAR LA CLASE ACCESOBD YA QUE PASA LO MISMO QUE EN FACHADA, DICHA CLASE AUN NO ESTA ENVUELTA EN UN MISMO PACKAGE
+	//TODO: TAMPOCO ENCUENTRA LA INTERFAZ DE POOL DE CONEXIONES YA QUE AUN NO TENEMOS TOODO COMPACTADO EN UN MISMO LUGAR, LO MISMO PARA LA CLASE DE VOTEMPORADA
+	//TODO: TAMBIEN ENLAZAR LAS CLASES DE EXCEPCIONES DE TIPO PERSISTENCIAEXCEPTION, TEMPORADAEXCEPTION, DRAGQUEENEXCEPTION EN UN PRINCIPIO
+	public void nuevaTemporada(VOTemporada voT) throws PersistenciaException, TemporadaException{
+		
+		 IPoolConexiones pool;
+		 IConexion icon= pool.obtenerConexion(true);
+		 String msgError;
+		 boolean errorPersistencia;
+		 boolean existeTemporada;
+		 
+		 try 
+		 {  
+			 int nroTemp = voT.getNroTemporada(); 
+			 int anio = voT.getAnio(); 
+			 int cantCapitulos = voT.getCantCapitulos(); 
+			
+			 AccesoBD abd= new AccesoBD();
+			 existeTemporada= abd.existeTemp(icon, nroTemp);
+			 if(!existeTemporada) 
+				 abd.insertarTemp(icon, nroTemp, anio, cantCapitulos); 
+			 else 
+				 msgError= "TEMPORADA YA EXISTENTE";
+			 
+			 pool.liberarConexion(icon, true);
+			 
+		 } 
+		 catch(Exception e)
+		 {
+		    pool.liberarConexion(icon, false); 
+		   	errorPersistencia= true; 
+		 	msgError = "ERROR AL INTENTAR ACCEDER A LOS DATOS";
+	 	 }
+		 finally
+		 { 
+			 if(existeTemporada) 
+				 throw new TemporadaException(msgError); 
+			 
+			 if(errorPersistencia) 
+				 throw new PersistenciaException(msgError); 
+		 }
+    }
 	
-	public void inscribirDragQueen(VODragQueen voD) {}
+	public void inscribirDragQueen(VODragQueen voD) {
+		
+		 IPoolConexiones pool;
+		 IConexion icon= pool.obtenerConexion(true);
+		 String msgError;
+		 boolean errorPersistencia;
+		 boolean existeTemporada;
+		 
+		 try 
+		 {  
+			 
+			 String nombre = voD.getNombre(); 
+			 int nroTemp = voD.getNroTemporada();  
+			
+			 AccesoBD abd= new AccesoBD();
+			 existeTemporada = abd.existeTemp(icon, nroTemp);
+			 if(existeTemporada) 
+				 abd.insertarDragQueen(icon, nombre, nroTemp);
+			 else 
+				 msgError= "NO EXISTE TEMPORADA A LA QUE ASIGNAR DICHA DRAGQUEEN";
+			 
+			 pool.liberarConexion(icon, true);
+			 
+		 }
+		 catch(Exception e)
+		 {
+			 	pool.liberarConexion(icon, false); 
+		 		errorPersistencia= true; 
+		 		msgError = "ERROR AL INTENTAR ACCEDER A LOS DATOS";
+	 	 }
+		 finally
+		 { 
+			 if(!existeTemporada) 
+				 throw new TemporadaException(msgError); 
+			 
+			 if(errorPersistencia) 
+				 throw new PersistenciaException(msgError); 
+		 }
+	}
 	
-	public List<VOTemporada> listarTemporadas(){}
+	public List<VOTemporada> listarTemporadas(){
+		
+		 IPoolConexiones pool;
+		 IConexion icon= pool.obtenerConexion(true);
+		 String msgError;
+		 boolean errorPersistencia;
+		 
+		 try 
+		 { 
+			 AccesoBD abd= new AccesoBD();
+			 
+			 List<VOTemporada> listaTemporadas = new ArrayList<VOTemporada>();
+			 
+			 //TODO: NO SE SI SE PUEDEN ASIGNAR LISTAS DE ESTA MANERA EN JAVA, O HABRIA QUE RECORRER LA LISTA QUE DEVUELVE LISTARTEMPORADAS PARA ARMAR UNA NUEVA LISTA PARA RETORNAR
+			 listaTemporadas = abd.listarTemporadas(icon);
+			 
+			 pool.liberarConexion(icon, true);
+			 
+			 return listaTemporadas;
+			 
+		 }
+		 catch(Exception e)
+		 {
+			 	pool.liberarConexion(icon, false); 
+		 		errorPersistencia= true; 
+		 		msgError = "ERROR AL INTENTAR ACCEDER A LOS DATOS";
+	 	 }
+		 finally
+		 { 
+			 if(errorPersistencia) 
+				 throw new PersistenciaException(msgError); 
+		 }
+		
+	}
 	
 	public List<VODragQueenVictorias> listarDragQueens(int nroTemp){}
 	
